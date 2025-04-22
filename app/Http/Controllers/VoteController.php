a5dd0ace4c0851b41ed4798ecf1a0db2190582f5
Jack Brookes <220094078@aston.ac.uk>
2025-04-22 14:21:23 +0100

    Fix the Forms, Store the Votes in the database

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class VoteController extends Controller
 {
+    
     public function addVote(Request $request){
         if($request['question']){
             $vote = Vote::updateOrCreate([
@@ -23,5 +24,6 @@ class VoteController extends Controller
                 'user_id' => Auth::id()],[
                 'value' => $request['vote'],
             ]);}
+            return redirect()->back();
 }
 }
